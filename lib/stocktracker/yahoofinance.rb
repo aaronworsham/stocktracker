91d3444906d491a30d8c1b56e9ef8bf1085f93f2
Aaron Worsham <aaronworsham@gmail.com>
2012-04-05 10:39:45 -0700

    added reattempt for reset connection error

--- lib/stocktracker/yahoofinance.rb
+++ lib/stocktracker/yahoofinance.rb
@@ -38,9 +38,18 @@ module YahooFinance
       self.results= get_current_quote
     end
 
-    def get_current_quote
-      current_map
-    rescue
+    def get_current_quote(attempt = 3)
+      if attempt > 0
+        puts "Attempt #{attempt} to pull quote for #{self.symbol}"
+        current_map || get_current_quote(attempt - 1)
+      else
+        raise 'Failed to get current quote within 3 attempts'
+      end
+    rescue Errno::ECONNRESET => e
+      puts "Connection Error #{e.message}"
+      get_current_quote(attempt - 1)
+    rescue => e
+      puts e.message
       nil
     end
 
